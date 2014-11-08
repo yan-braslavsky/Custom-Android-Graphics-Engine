763876e8b8569a6a1ef75ee004e9edfac1ef51cb
Yan Braslavskiy <iveliusster@gmail.com>
2014-11-08 10:05:09 +0100

    vertex data array is now cached , no new instances allocated each frame.

--- GlEngine/src/main/java/com/yan/glengine/nodes/YANTexturedNode.java
+++ GlEngine/src/main/java/com/yan/glengine/nodes/YANTexturedNode.java
@@ -7,13 +7,21 @@ package com.yan.glengine.nodes;
 import com.yan.glengine.assets.atlas.YANTextureRegion;
 import com.yan.glengine.programs.YANTextureShaderProgram;
 
-public class YANTexturedNode extends YANBaseNode<YANTextureShaderProgram>{
+public class YANTexturedNode extends YANBaseNode<YANTextureShaderProgram> {
 
+    private static final int VALUES_PER_VERTEX = POSITION_COMPONENT_COUNT + TEXTURE_COORDINATES_COMPONENT_COUNT;
+    private static final int VERTICES_COUNT = 6;
     private YANTextureRegion mTextureRegion;
+
+    //vertex data is a cached float array with a fixed size.
+    //that array size is not subject to change , it will always have only position and
+    //texture coordinates data.
+    private float[] mVertexData;
 
     public YANTexturedNode(YANTextureRegion textureRegion) {
         super();
         mTextureRegion = textureRegion;
+        mVertexData = new float[VALUES_PER_VERTEX * VERTICES_COUNT];
     }
 
     public YANTextureRegion getTextureRegion() {
@@ -45,30 +53,48 @@ public class YANTexturedNode extends YANBaseNode<YANTextureShaderProgram>{
         float halfWidth = getSize().getX() / 2f;
         float halfHeight = getSize().getY() / 2f;
 
-        return new float[]{
-                // Order of coordinates: X, Y, U, V
-                // Triangle Fan
+        // Order of coordinates: X, Y, U, V
+        // Triangle Fan
 
-                //first vertex (center)
-                0f, 0f, mTextureRegion.getU0() + ((mTextureRegion.getU1() - mTextureRegion.getU0()) / 2), mTextureRegion.getV0() + ((mTextureRegion.getV1() - mTextureRegion.getV0()) / 2),
+        //first vertex (center)
+        mVertexData[0] = 0f;
+        mVertexData[1] = 0f;
+        mVertexData[2] = mTextureRegion.getU0() + ((mTextureRegion.getU1() - mTextureRegion.getU0()) / 2);
+        mVertexData[3] = mTextureRegion.getV0() + ((mTextureRegion.getV1() - mTextureRegion.getV0()) / 2);
 
-                //second vertex (bottom left)
-                -halfWidth, -halfHeight, mTextureRegion.getU0(), mTextureRegion.getV0(),
+        //second vertex (bottom left)
+        mVertexData[4] = -halfWidth;
+        mVertexData[5] = -halfHeight;
+        mVertexData[6] = mTextureRegion.getU0();
+        mVertexData[7] = mTextureRegion.getV0();
 
-                //third vertex (top left)
-                -halfWidth, halfHeight, mTextureRegion.getU0(), mTextureRegion.getV1(),
+        //third vertex (top left)
+        mVertexData[8] = -halfWidth;
+        mVertexData[9] = halfHeight;
+        mVertexData[10] = mTextureRegion.getU0();
+        mVertexData[11] = mTextureRegion.getV1();
 
-                //fourth vertex (top right)
-                halfWidth, halfHeight, mTextureRegion.getU1(), mTextureRegion.getV1(),
+        //fourth vertex (top right)
+        mVertexData[12] = halfWidth;
+        mVertexData[13] = halfHeight;
+        mVertexData[14] = mTextureRegion.getU1();
+        mVertexData[15] = mTextureRegion.getV1();
 
-                //fifth vertex (bottom right)
-                halfWidth, -halfHeight, mTextureRegion.getU1(), mTextureRegion.getV0(),
+        //fifth vertex (bottom right)
+        mVertexData[16] = halfWidth;
+        mVertexData[17] = -halfHeight;
+        mVertexData[18] = mTextureRegion.getU1();
+        mVertexData[19] = mTextureRegion.getV0();
 
-                //sixth vertex (bottom left)
-                -halfWidth, -halfHeight, mTextureRegion.getU0(), mTextureRegion.getV0(),
-        };
+        //sixth vertex (bottom left)
+        mVertexData[20] = -halfWidth;
+        mVertexData[21] = -halfHeight;
+        mVertexData[22] = mTextureRegion.getU0();
+        mVertexData[23] = mTextureRegion.getV0();
+
+        return mVertexData;
+
     }
-
 
 
 }
