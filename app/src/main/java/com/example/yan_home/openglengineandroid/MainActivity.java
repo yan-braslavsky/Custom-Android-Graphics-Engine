7ae2d1f1ffcf7bb83c030956cc6b5ac8a7b1379d
Yan Braslavskiy <iveliusster@gmail.com>
2014-12-25 08:30:51 +0100

    Main Activity extracted to Engine Activity Base

--- app/src/main/java/com/example/yan_home/openglengineandroid/MainActivity.java
+++ app/src/main/java/com/example/yan_home/openglengineandroid/MainActivity.java
@@ -1,195 +1,23 @@
 package com.example.yan_home.openglengineandroid;
 
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.content.Context;
-import android.content.pm.ConfigurationInfo;
-import android.opengl.GLSurfaceView;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
-import android.widget.Toast;
-
 import com.example.yan_home.openglengineandroid.screens.ConnectionTestScreen;
-import com.yan.glengine.EngineWrapper;
-import com.yan.glengine.assets.YANAssetManager;
-import com.yan.glengine.setup.YANEngineSetup;
+import com.yan.glengine.EngineActivity;
+import com.yan.glengine.renderer.YANGLRenderer;
+import com.yan.glengine.screens.YANIScreen;
 
 import java.util.HashMap;
 
-public class MainActivity extends Activity {
-
-    private static EngineWrapper renderer;
-    private GLSurfaceView glSurfaceView;
-
-    public MainActivity() {
-        super();
+public class MainActivity extends EngineActivity {
+    @Override
+    protected HashMap<Integer, Integer> onCreateAssetsMap() {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(R.raw.ui_atlas, R.drawable.ui_atlas);
+        return map;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (renderer == null) {
-            renderer = new EngineWrapper(GLEngineApp.getAppContext());
-
-            HashMap<Integer, Integer> map = new HashMap<>();
-            map.put(R.raw.ui_atlas, R.drawable.ui_atlas);
-
-            YANAssetManager.getInstance().preloadAssets(map, new YANAssetManager.YANAssetManagerListener() {
-                @Override
-                public void onAssetsPreloaded() {
-
-                    //setup the tween engine
-                    YANEngineSetup.setupTweenEngine();
-
-                    //set the first screen
-                    EngineWrapper.getRenderer().setActiveScreen(new ConnectionTestScreen(EngineWrapper.getRenderer()));
-
-                    //init the engine
-                    init();
-                }
-            });
-        } else {
-
-            //init the engine
-            init();
-        }
-
+    protected YANIScreen onCreateStartScreen(YANGLRenderer renderer) {
+        return new ConnectionTestScreen(renderer);
     }
 
-    // This snippet hides the system bars.
-    public void hideSystemUI() {
-        // Set the IMMERSIVE flag.
-        // Set the content to appear under the system bars so that the content
-        // doesn't resize when the system bars hide and show.
-        glSurfaceView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
-    }
-
-
-    private void init() {
-
-        glSurfaceView = new GLSurfaceView(this);
-
-        hideSystemUI();
-
-        // Check if the system supports OpenGL ES 2.0.
-        ActivityManager activityManager =
-                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        ConfigurationInfo configurationInfo = activityManager
-                .getDeviceConfigurationInfo();
-        // Even though the latest emulator supports OpenGL ES 2.0,
-        // it has a bug where it doesn't set the reqGlEsVersion so
-        // the above check doesn't work. The below will detect if the
-        // app is running on an emulator, and assume that it supports
-        // OpenGL ES 2.0.
-        final boolean supportsEs2 =
-                configurationInfo.reqGlEsVersion >= 0x20000
-                        || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
-                        && (Build.FINGERPRINT.startsWith("generic")
-                        || Build.FINGERPRINT.startsWith("unknown")
-                        || Build.MODEL.contains("google_sdk")
-                        || Build.MODEL.contains("Emulator")
-                        || Build.MODEL.contains("Android SDK built for x86")));
-
-        if (supportsEs2) {
-            // ...
-            // Request an OpenGL ES 2.0 compatible context.
-            glSurfaceView.setEGLContextClientVersion(2);
-
-            // Assign our renderer.
-            glSurfaceView.setRenderer(renderer);
-        } else {
-            /*
-             * This is where you could create an OpenGL ES 1.x compatible
-             * renderer if you wanted to support both ES 1 and ES 2. Since
-             * we're not doing anything, the app will crash if the device
-             * doesn't support OpenGL ES 2.0. If we publish on the market, we
-             * should also add the following to AndroidManifest.xml:
-             *
-             * <uses-feature android:glEsVersion="0x00020000"
-             * android:required="true" />
-             *
-             * This hides our app from those devices which don't support OpenGL
-             * ES 2.0.
-             */
-            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
-                    Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        glSurfaceView.setOnTouchListener(new OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event != null) {
-                    // Convert touch coordinates into normalized device
-                    // coordinates, keeping in mind that Android's Y
-                    // coordinates are inverted.
-                    final float normalizedX =
-                            (event.getX() / (float) v.getWidth());
-                    final float normalizedY =
-                            ((event.getY() / (float) v.getHeight()));
-
-                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                        glSurfaceView.queueEvent(new Runnable() {
-                            @Override
-                            public void run() {
-                                renderer.handleTouchDown(
-                                        normalizedX, normalizedY);
-                            }
-                        });
-                    } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-                        glSurfaceView.queueEvent(new Runnable() {
-                            @Override
-                            public void run() {
-                                renderer.handleTouchDrag(
-                                        normalizedX, normalizedY);
-                            }
-                        });
-                    } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                        glSurfaceView.queueEvent(new Runnable() {
-                            @Override
-                            public void run() {
-                                renderer.handleTouchUp(
-                                        normalizedX, normalizedY);
-                            }
-                        });
-                    }
-
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        });
-
-        setContentView(glSurfaceView);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (glSurfaceView != null) {
-            glSurfaceView.onPause();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (glSurfaceView != null) {
-            glSurfaceView.onResume();
-        }
-    }
 }
